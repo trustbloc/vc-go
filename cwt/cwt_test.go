698457629a56acdf418c5f7f466d71875e004248
Stas Dm <iqpirat@gmail.com>
2024-07-15 14:34:11 +0200

    fix: cwt proof test

--- cwt/cwt_test.go
+++ cwt/cwt_test.go
@@ -10,6 +10,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
 	_ "crypto/sha256"
 	"encoding/hex"
 	"errors"
@@ -21,14 +22,16 @@ import (
 	"github.com/veraison/go-cose"
 
 	"github.com/trustbloc/vc-go/cwt"
+	"github.com/trustbloc/vc-go/proof"
 	"github.com/trustbloc/vc-go/proof/checker"
+	cwt2 "github.com/trustbloc/vc-go/verifiable/cwt"
 )
 
 const (
 	exampleCWT = "d2844aa201260445616e794944a05848a4066a313730363730363932370a746b596362437876656c6531706e393459704b6a44016b746573742d636c69656e740376687474703a2f2f3132372e302e302e313a3630343133589ad2844aa201260445616e794944a05848a4066a313730363730363932370a746b596362437876656c6531706e393459704b6a44016b746573742d636c69656e740376687474703a2f2f3132372e302e302e313a363034313358409d890356a79ebb3d53be14e98e875a870b4e3af426e0a847fd94013a378eae6376cbacb115a3296ba67622cc50dcae8de94c752f63afc7b7782c7ad45380b424" //nolint:lll
 )
 
-func TestCreateProof(t *testing.T) {
+func createTestProof() (string, error) {
 	data := map[int]interface{}{
 		1:  "test-client",
 		3:  "http://127.0.0.1:60413",
@@ -36,29 +39,61 @@ func TestCreateProof(t *testing.T) {
 		10: "kYcbCxvele1pn94YpKjD",
 	}
 
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	encoded, err := cbor.Marshal(data)
-	assert.NoError(t, err)
+	if err != nil {
+		return "", err
+	}
+
+	parsedPubKey, err := cose.NewKeyFromPublic(&privateKey.PublicKey)
+	if err != nil {
+		return "", err
+	}
+
+	keyBytes, err := parsedPubKey.MarshalCBOR()
+	if err != nil {
+		return "", err
+	}
+
+	pubKeyStr := hex.EncodeToString(keyBytes)
 
 	msg := &cose.Sign1Message{
 		Headers: cose.Headers{
 			Protected: cose.ProtectedHeader{
-				cose.HeaderLabelAlgorithm: cose.AlgorithmES256,
-				cose.HeaderLabelKeyID:     []byte("anyID"),
+				cose.HeaderLabelAlgorithm:   cose.AlgorithmES256,
+				cose.HeaderLabelContentType: proof.CWTProofType,
+				proof.COSEKeyHeader:         pubKeyStr,
 			},
 		},
 		Payload: encoded,
 	}
 
-	signature, err := SignP256(encoded, msg.Headers)
-	assert.NoError(t, err)
+	signData, err := cwt2.GetProofValue(msg)
+	if err != nil {
+		return "", err
+	}
+
+	signature, err := SignP256WithKey(signData, msg.Headers, privateKey)
+	if err != nil {
+		return "", err
+	}
 
 	msg.Signature = signature
 
 	final, err := cbor.Marshal(msg)
-	assert.NoError(t, err)
+	if err != nil {
+		return "", err
+	}
 
 	hexVal := hex.EncodeToString(final)
-	assert.NotEmpty(t, hexVal)
+
+	return hexVal, nil
+}
+
+func TestCreateProof(t *testing.T) {
+	result, err := createTestProof()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, result)
 }
 
 func TestParse(t *testing.T) {
@@ -70,14 +105,16 @@ func TestParse(t *testing.T) {
 		Unprotected: cose.UnprotectedHeader{},
 	}
 
+	testProof, err := createTestProof()
+	assert.NoError(t, err)
+
 	t.Run("success", func(t *testing.T) {
-		input, decodeErr := hex.DecodeString(exampleCWT)
+		input, decodeErr := hex.DecodeString(testProof)
 		assert.NoError(t, decodeErr)
 
 		proofChecker := NewMockProofChecker(gomock.NewController(t))
 		proofChecker.EXPECT().CheckCWTProof(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
 			DoAndReturn(func(request checker.CheckCWTProofRequest, expectedIssuer string, message, sign []byte) error {
-				assert.Equal(t, "anyID", request.KeyID)
 				assert.Equal(t, cose.AlgorithmES256, request.Algo)
 				assert.NotNil(t, message)
 				assert.Equal(t, "test-client", expectedIssuer)
@@ -93,7 +130,7 @@ func TestParse(t *testing.T) {
 	})
 
 	t.Run("invalid proof", func(t *testing.T) {
-		input, decodeErr := hex.DecodeString(exampleCWT)
+		input, decodeErr := hex.DecodeString(testProof)
 		assert.NoError(t, decodeErr)
 
 		proofChecker := NewMockProofChecker(gomock.NewController(t))
@@ -165,17 +202,19 @@ func TestParse(t *testing.T) {
 		assert.ErrorContains(t, cwt.CheckProof(&cose.Sign1Message{}, nil, nil, nil, nil),
 			"algorithm not found")
 	})
+}
 
-	t.Run("no key", func(t *testing.T) {
-		assert.ErrorContains(t, cwt.CheckProof(&cose.Sign1Message{
-			Headers: cose.Headers{
-				Protected: cose.ProtectedHeader{
-					cose.HeaderLabelAlgorithm: cose.AlgorithmES256,
-				},
-			},
-		}, nil, nil, nil, nil),
-			"check cwt failure: COSE_Key header is required")
-	})
+func SignRS256(
+	data []byte,
+	headers cose.Headers,
+	privateKey *rsa.PrivateKey,
+) ([]byte, error) {
+	signer, err := cose.NewSigner(cose.AlgorithmRS256, privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return cose.Sign1(rand.Reader, signer, headers, data, nil)
 }
 
 func SignP256(data []byte, headers cose.Headers) ([]byte, error) {
@@ -184,13 +223,18 @@ func SignP256(data []byte, headers cose.Headers) ([]byte, error) {
 		return nil, err
 	}
 
-	signer, err := cose.NewSigner(cose.AlgorithmES256, privateKey)
+	return SignP256WithKey(data, headers, privateKey)
+}
+
+func SignP256WithKey(
+	data []byte,
+	headers cose.Headers,
+	key *ecdsa.PrivateKey,
+) ([]byte, error) {
+	signer, err := cose.NewSigner(cose.AlgorithmES256, key)
 	if err != nil {
 		return nil, err
 	}
 
-	// create message header
-
-	// sign and marshal message
 	return cose.Sign1(rand.Reader, signer, headers, data, nil)
 }
