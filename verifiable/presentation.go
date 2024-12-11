bfda7bf6000e37362e313f8b2a639c4ab02ec48d
Stas Dm <iqpirat@gmail.com>
2024-12-11 13:42:09 +0100

    fix: subject

--- verifiable/presentation.go
+++ verifiable/presentation.go
@@ -740,9 +740,9 @@ func validateHolder(
 		return nil
 	}
 
-	if holder == "" {
-		return nil
-	}
+	//if holder == "" {
+	//	return nil
+	//}
 
 	existingMethods := make(map[string]struct{})
 
@@ -754,6 +754,18 @@ func validateHolder(
 		}
 
 		existingMethods[strings.Split(verMethod, "#")[0]] = struct{}{}
+	}
+
+	subjects := make(map[string]struct{})
+	for _, cred := range creds {
+		for _, sub := range cred.Contents().Subject {
+			subjects[sub.ID] = struct{}{}
+		}
+	}
+
+	if len(subjects) == 0 ||
+		len(subjects) > 1 { // skip holder check if there are multiple subjects in credentials
+		return nil
 	}
 
 	for _, cred := range creds {
